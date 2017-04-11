061271736092436cf1cca640dbc17d594fb3cd14
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-11 20:51:48 +0000

    finish express ex and upload js

---
+++ express_server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+let app = express();
+// default port is 8080
+let PORT = process.env.PORT || 8080;
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+app.get("/", (req, res) => {
+  res.end("Hello");
+});
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
