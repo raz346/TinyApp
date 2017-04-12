43a686c32af2cf59f92e2c1c149635d2afb773bf
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-12 00:28:26 +0000

    create urls_new.ejs in views and add .post to the main js file

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 let app = express();
 // default port is 8080
 let PORT = 8080;
@@ -13,20 +14,30 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+// Add a new route to urls_index to print the shorten and full url
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
+// Add a new route to urls_show to print each urls based on given id 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { 
     shortURL: req.params.id,
-    longURL: urlDatabase[req.params.id]
+    longURL: urlDatabase[shortURL]
   };
   res.render("urls_show", templateVars);
 })
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
 });
